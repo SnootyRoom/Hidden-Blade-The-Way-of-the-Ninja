8ddd7f7cfcef1b00657c873f1973bc322e27df4b
Bogdan <snootyroom25@gmail.com>
2024-02-05 20:25:00 +0500

    Version 1.1

--- main.py
+++ main.py
@@ -190,7 +190,143 @@ def start_screen():
         clock.tick(FPS)
 
 
-def game_over_screen():
+def level1():
+    lev = True
+
+    score = 0
+    platform = Platform(400 // 2 - 100, 700 - 100, 100)
+    platform_group.add(platform)
+
+    bg_scroll = 0
+    img = pygame.transform.scale(back_img, (400, 700))
+
+    run = True
+    while run:
+
+        clock.tick(FPS)
+
+        scroll = player.move()
+
+        bg_scroll += scroll
+        if bg_scroll >= 600:
+            bg_scroll = 0
+
+        draw_background(bg_scroll)
+        pygame.draw.line(screen, (255, 255, 255), (0, SCROLL_THRESH), (700, SCROLL_THRESH))
+        screen.blit(img, (0, 0))
+
+        if len(platform_group) < MAX_PLATFORMS:
+            p_w = random.randint(40, 60)
+            p_x = random.randint(0, 400 - p_w)
+            p_y = platform.rect.y - random.randint(80, 100)
+            platform = Platform(p_x, p_y, p_w)
+            platform_group.add(platform)
+
+        if scroll > 0:
+            score += 1
+
+        platform_group.update(scroll)
+        platform_group.draw(screen)
+        player.draw()
+        pygame.draw.rect(screen, pygame.Color(255, 209, 220), (400, 0, 300, 700))
+
+        font = pygame.font.Font(None, 50)
+        rend = font.render(f"Счёт: {score}", 1, pygame.Color((93, 155, 155)))
+        intro_rect = rend.get_rect()
+        intro_rect.top = 70
+        intro_rect.x = 420
+        screen.blit(rend, intro_rect)
+
+        back_btn = "Назад"
+        font = pygame.font.Font(None, 50)
+        rend = font.render(back_btn, 1, pygame.Color((93, 155, 155)))
+        intro_rect_b = rend.get_rect()
+        intro_rect_b.top = 650
+        intro_rect_b.x = 570
+        intro_rect_b.width = 125
+        pygame.draw.rect(screen, pygame.Color(229, 228, 226), intro_rect_b)
+        pygame.draw.rect(screen, pygame.Color(93, 155, 155), intro_rect_b, 4)
+        screen.blit(rend, intro_rect_b)
+
+        if player.rect.top >= 600:
+            game_over_screen(lev)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if score >= 2000:
+                game_over_screen_Levels()
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.pos[0] >= intro_rect_b.x and event.pos[0] <= intro_rect_b.right and \
+                        event.pos[1] >= intro_rect_b.top and event.pos[0] <= intro_rect_b.bottom:
+                    levels_screen()
+        pygame.display.update()
+
+
+def level2():
+    pass
+
+
+def level3():
+    pass
+
+
+def level4():
+    pass
+
+
+def level5():
+    pass
+
+
+def game_over_screen(lev=False):
+    lev = lev
+    fon = pygame.transform.scale(load_image('gameover.jpg'), screen_size)
+    screen.blit(fon, (0, 0))
+
+    font = pygame.font.Font(None, 125)
+    rend = font.render(f"Вы упали", 1, pygame.Color((127, 199, 255)))
+    intro_rect = rend.get_rect()
+    intro_rect.top = 20
+    intro_rect.x = 175
+    screen.blit(rend, intro_rect)
+
+    if not lev:
+        back_btn = "Меню"
+        font = pygame.font.Font(None, 100)
+    else:
+        back_btn = "К уровням"
+        font = pygame.font.Font(None, 60)
+
+    rend = font.render(back_btn, 1, pygame.Color((93, 155, 155)))
+    intro_rect_b = rend.get_rect()
+    intro_rect_b.top = 400
+    intro_rect_b.x = 200
+    intro_rect_b.width = 250
+    pygame.draw.rect(screen, pygame.Color(229, 228, 226), intro_rect_b)
+    pygame.draw.rect(screen, pygame.Color(93, 155, 155), intro_rect_b, 4)
+    screen.blit(rend, intro_rect_b)
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.pos[0] >= intro_rect_b.x and event.pos[0] <= intro_rect_b.right and \
+                        event.pos[1] >= intro_rect_b.top and event.pos[0] <= intro_rect_b.bottom:
+                    if not lev:
+                        start_screen()
+                    else:
+                        levels_screen()
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def game_over_screen_Levels():
     fon = pygame.transform.scale(load_image('gameover.jpg'), screen_size)
     screen.blit(fon, (0, 0))
 
@@ -212,19 +348,6 @@ def game_over_screen():
     pygame.draw.rect(screen, pygame.Color(93, 155, 155), intro_rect_b, 4)
     screen.blit(rend, intro_rect_b)
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.pos[0] >= intro_rect_b.x and event.pos[0] <= intro_rect_b.right and \
-                        event.pos[1] >= intro_rect_b.top and event.pos[0] <= intro_rect_b.bottom:
-                    start_screen()
-
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
 def training_screen():
     pass
 
@@ -233,7 +356,6 @@ def infinity_game():
     con = sqlite3.connect('data\statistic')
     cur = con.cursor()
     high_score = int(cur.execute('SELECT High_score FROM Statistic WHERE id = 1').fetchone()[0])
-    game_over = False
 
     score = 0
     platform = Platform(400 // 2 - 100, 700 - 100, 100)
@@ -368,6 +490,21 @@ def levels_screen():
                 if event.pos[0] >= intro_rect_b.x and event.pos[0] <= intro_rect_b.right and \
                         event.pos[1] >= intro_rect_b.top and event.pos[0] <= intro_rect_b.bottom:
                     start_screen()
+                if event.pos[0] >= buttons[0].x and event.pos[0] <= buttons[0].right and \
+                        event.pos[1] >= buttons[0].top and event.pos[0] <= buttons[0].bottom:
+                    level1()
+                if event.pos[0] >= buttons[1].x and event.pos[0] <= buttons[1].right and \
+                        event.pos[1] >= buttons[1].top and event.pos[0] <= buttons[1].bottom:
+                    level2()
+                if event.pos[0] >= buttons[2].x and event.pos[0] <= buttons[2].right and \
+                        event.pos[1] >= buttons[2].top and event.pos[0] <= buttons[2].bottom:
+                    level3()
+                if event.pos[0] >= buttons[3].x and event.pos[0] <= buttons[3].right and \
+                        event.pos[1] >= buttons[3].top and event.pos[0] <= buttons[3].bottom:
+                    level4()
+                if event.pos[0] >= buttons[4].x and event.pos[0] <= buttons[4].right and \
+                        event.pos[1] >= buttons[4].top and event.pos[0] <= buttons[4].bottom:
+                    level5()
         pygame.display.flip()
         clock.tick(FPS)
 
